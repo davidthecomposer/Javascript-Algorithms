cc302ac67d18bcc7ced9875dd514cb6ae040e237
unknown <davidthecomposer@gmail.com>
2019-06-06 10:02:39 -0600

    Added 4th Algorithm

---
+++ 4. Intro From Name.js
@@ -0,0 +1,6 @@
+////////////////////////// Intro-From-Name  ///////////////////////////////
+const introFromName = (firstName, lastName) => {
+
+    return `Hi, my name is ${firstName} ${lastName}. Nice to meet you!`;
+};
+console.log(introFromName('David', 'Campbell'));
