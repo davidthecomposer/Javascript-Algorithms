ff85c45512c957b1229f8d2574f09971ffaa3d50
unknown <davidthecomposer@gmail.com>
2019-06-06 09:49:44 -0600

    Added 3rd Algorithm.

---
+++ 3. Iterate Strings.js
@@ -0,0 +1,6 @@
+const exampleArray = ['first', 'second', 'third', 'fourth', 'fifth'];
+// const exampleArray = ['Hello', 'Bye', 'Hi', 'Heya', 'Sexy Beast'];
+
+for (const word of exampleArray){
+  console.log(word);
+}
